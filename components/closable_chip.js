6209bf080e40c547c08f915d748176445655d915
Hayden Gorringe <hayden.gorringe@gmail.com>
2024-03-25 17:57:20 +0000

    fixed ability to close chip

--- components/closable_chip.js
+++ components/closable_chip.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function ClosableChip({chip_text, remove_keywords, index=0 }) {
     return (
-        <div className="flex mr-3 cursor-pointer" onClick={() => {remove_keywords()}}>
+        <div className="flex mr-3 cursor-pointer">
             <Chip chip_text={chip_text} index={index}/>
             <div className="my-auto">
-                <Image src="images/svgs/cancel.svg" width={24} height={24}></Image>
+                <Image src="images/svgs/cancel.svg" width={24} height={24} onClick={() => {remove_keywords(chip_text)}}></Image>
             </div>
         </div>
     )
