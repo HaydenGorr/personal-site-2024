8b1f27b64f5bc0b3ce4e75e0b0d45ce040dab878
Hayden Gorringe <33552561+HaydenGorr@users.noreply.github.com>
2024-03-31 23:16:41 +0100

    scrollable header (#6)

--- components/buttons/MB_Button.js
+++ components/buttons/MB_Button.js
@@ -18,13 +18,12 @@ export default function MB_Button({ given_href="", text="", image_src="", lowerc
 
     const getStyles = () => {
         if (injected_styles) return injected_styles;
-        else return `${colour} btn-primary h-10 flex Neo-Brutal-White w-fit active:MB_clicked active:shadow-MB_clicked active:bg-MB_clicked`
-
+        else return `${colour} w-fit btn-primary flex Neo-Brutal-White active:MB_clicked active:shadow-MB_clicked active:bg-MB_clicked`
     }
 
     const getParentElement = (child) => {
         if (given_href[0] == '/' ) return <Link onClick={handleClick} class={getStyles()} href={given_href}>{child}</Link>
-        else return <a onClick={handleClick} class={getStyles()} href={given_href} target='_blank'>{child}</a>
+        else return <a onClick={handleClick} className={getStyles()} href={given_href} target='_blank'>{child}</a>
     }
 
     return (
