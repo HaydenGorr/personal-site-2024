69a5a3342a8bfca0d0b344179a4209235ba857e4
Hayden Gorringe <hayden.gorringe@Gmail.com>
2024-04-01 11:05:15 +0100

    Update [id].js

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -23,23 +23,5 @@ export default function Article() {
                 </div>
             </div>
         </Layout>
-        
     );
 }
-
-// export async function getServerSideProps({ params }) {
-//     const { id } = params;
-  
-//     // Construct the file path
-//     const filePath = path.join(process.cwd(), 'public', 'articles', `${id}.md`);
-
-//     // Read the markdown file content
-//     const markdown = fs.readFileSync(filePath, 'utf8');
-    
-//     // Convert the markdown to HTML
-//     const html = marked(markdown);
-
-//     return {
-//         props: { post: html},
-//     };
-//   }
