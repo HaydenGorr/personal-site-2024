74afd86a30f379cc00a25ee1b9741cb5c09619d9
Hayden Gorringe <hayden.gorringe@Gmail.com>
2024-04-13 00:56:53 +0100

    Update image_wrapper.js

--- webserver/components/image_wrapper.js
+++ webserver/components/image_wrapper.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ImageWrapper({ src }) {
     return (
         <div>
-            <Image className={"Neo-Brutal-White"} src={`${process.env.NEXT_PUBLIC_LOCAL_ACCESS_CMS}/${src}`} width={1000} height={1000}/>
+            <Image className={"Neo-Brutal-White"} src={`${process.env.NEXT_PUBLIC_USER_ACCESS_CMS}/${src}`} width={1000} height={1000}/>
         </div>
     )
 }
