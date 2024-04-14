d961404f187258f849e4aa280b292d1fb860a993
Hayden Gorringe <hayden.gorringe@gmail.com>
2024-04-14 20:43:34 +0100

    Update custom_link.js

--- webserver/components/custom_link.js
+++ webserver/components/custom_link.js
@@ -1,23 +1,33 @@
-import React from 'react';
 import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
 
 const CustomLink = ({ href, children }) => {
+  const [imageURL, setImageURL] = useState('/images/svgs/link_icon.svg');
 
-  const getImageURL = () => {
-    try {
-      const url = new URL(href);
-      const faviconUrl = `${url.origin}/favicon.ico`;
-      return faviconUrl.toString();
-    } catch (e) {
-      return '/images/svgs/link.svg';
-    }
-  };
+  useEffect(() => {
+    const getImageURL = async () => {
+      try {
+        const url = new URL(href);
+        const faviconUrl = `${url.origin}/favicon.ico`;
+        const res = await fetch(faviconUrl);
+        if (res.ok) {
+          setImageURL(faviconUrl.toString());
+        }
+      } catch (e) {
+        console.log(e)
+        return
+      }
+    };
+
+    getImageURL();
+  }, [href]);
+
 
   return (
     <div className="custom-link bg-gray-200 px-1 rounded-lg">
       <div className="mr-1">
         <div className="inlineimg">
-          <Image src={getImageURL()} width={14} height={14} />
+          <Image src={imageURL} width={14} height={14} />
         </div>
       </div>
       <a href={href} className="link" target="_blank">
