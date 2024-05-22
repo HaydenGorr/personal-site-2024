a7fbb403582b4eb80b9c5ac677f8d4a6797683ee
Hayden Gorringe <hayden.gorringe@Gmail.com>
2024-05-22 12:20:31 +0100

    update

--- webserver/components/chat_bubble.js
+++ webserver/components/chat_bubble.js
@@ -1,15 +1,10 @@
+import ReactMarkdown from 'react-markdown';
 
 export default function ChatBubble({key, incoming, inText="Example text"}) {
 
-    const baseClasses = "p-4 my-2 border-black border-2";
-    const incomingClasses = "bg-black self-start default_text_colour_white";
-    const outgoingClasses = "default_colour self-end text-black";
-  
-    const classes = `${baseClasses} ${incoming === false ? outgoingClasses : incomingClasses}`;
-
     return (
-        <div className={`p-3 my-2 border-2 shadow-MB prose ${incoming === false ? "Neo-Brutal-White self-end ml-6" : "self-start Neo-Brutal mr-6"}`}>
-            {inText}
+        <div className={`Neo-Brutal-White p-3 my-2 border-2 shadow-MB prose ${incoming === false ? "self-end ml-6" : "self-start mr-6"}`}>
+            <ReactMarkdown children={inText} />
         </div>
     );
   }
