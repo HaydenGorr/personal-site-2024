b7fbfffbb63e9d2b7d5473973334ed2ec246db64
Hayden Gorringe <hayden.gorringe@Gmail.com>
2024-07-22 12:05:19 +0100

    Update custom_link.js

--- webserver/components/custom_link.js
+++ webserver/components/custom_link.js
@@ -20,7 +20,7 @@ const CustomLink = ({ href, children, backgroundColour }) => {
   });
 
   return (
-    <div className={`custom-link px-1 rounded-lg transition-colors duration-500`} style={{backgroundColor: getTirtaryColour(backgroundColour)}}>
+    <div className={`custom-link px-1 rounded-lg transition-colors duration-500`} style={{backgroundColor: getTirtaryColour(backgroundColour), color: getTextColour(backgroundColour)}}>
       <div className="mr-1">
         <div className="inlineimg">
           <Image src={faviconUrl} width={14} height={14} />
