9a4312e1f0ab37bc58a2ec85d694c94924188b83
Hayden Gorringe <hayden.gorringe@Gmail.com>
2024-08-13 12:58:46 +0100

    Update very_condensed_article.js

--- webserver/components/portfolio/very_condensed_article.js
+++ webserver/components/portfolio/very_condensed_article.js
@@ -36,15 +36,16 @@ export default function VeryCondensedArticle({ name, desc, type, has_best_articl
                 </p>
             </div>
 
-            <div className="flex-shrink-0 flex ml-auto flex-col space-y-4">
-                <button className={`${getTypeColour(type)} p-1 px-2 rounded-2xl whitespace-nowrap flex items-center justify-center`}>
-                    <Image className="mx-1" width={20} height={20} src={getTypeImage("asd")}/>
-                    <p className="mr-1 font-semibold text-xs">Full</p>
+            <div className="flex-shrink-0 flex ml-auto flex-col space-y-1">
+                {/* {has_best_article && <p className="flex justify-center m-0 text-xs">Read the</p>} */}
+                <button className={`${getTypeColour(type)} p-1 px-2 rounded-2xl whitespace-nowrap flex items-center justify-center`} onClick={() => go_to_article(source)}>
+                    <Image className="mx-1" width={20} height={20} src={getTypeImage(type)}/>
+                    <p className="mr-1 font-semibold text-xs">{has_best_article ? "Whole thing" : "Read"}</p>
                 </button>
-                {/* {has_best_article && <p className="flex justify-center m-0">or</p>} */}
-                {has_best_article && <button onClick={() => go_to_article(type)} className={`${getTypeColour(type)}  flex p-1 rounded-2xl whitespace-nowrap items-center justify-center`}>
+                {/* {has_best_article && <p className="flex justify-center m-0 text-xs">or the</p>} */}
+                {has_best_article && <button onClick={() => go_to_article(source+"_")} className={`${getTypeColour(type)}  flex p-1 rounded-2xl whitespace-nowrap items-center justify-center`}>
                     <Image className="mx-1" width={20} height={20} src={"/images/svgs/star.svg"}/>
-                    <p className="mr-2 font-semibold text-xs">Best Bit</p>
+                    <p className="mr-2 font-semibold text-xs">Best bit</p>
                 </button>}
             </div>
             
