602ed0a955fcab594e0ad6f5ed65089d988dd51f
Hayden Gorringe <33552561+HaydenGorr@users.noreply.github.com>
2024-11-25 11:46:39 +0000

    Update login.js

--- webserver/pages/admin/login.js
+++ webserver/pages/admin/login.js
@@ -131,6 +131,14 @@ export default function Login() {
                         >
                             Login
                         </button>
+
+                        <button
+                            type="button"
+                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 mr-3"
+                            onClick={() => {send_sign_up_request(Uusername, Upassword)}}
+                        >
+                            sign up
+                        </button>
                     </div>
                 </form>
             </div>
