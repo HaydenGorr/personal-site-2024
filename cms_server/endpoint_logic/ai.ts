38968669f230217368104bd66804ef81fe6b747b
Hayden Gorringe <33552561+HaydenGorr@users.noreply.github.com>
2024-12-11 10:55:15 +0000

    update

--- cms_server/endpoint_logic/ai.ts
+++ cms_server/endpoint_logic/ai.ts
@@ -1,44 +1,100 @@
+import { error } from "console";
 import { app, anthropic } from "../express.js";
+import { get_all_ready_articles } from "../utils/mongo_utils/article.js";
+import { api_return_schema, article, chip } from "../interfaces/interfaces.js";
+import { get_chat_bot_system_prompt, get_tag_finder_system_prompt } from '../utils/ai_utils.js'
+import { get_unique_chips } from "../utils/mongo_utils/chips.js";
 
 
-app.post('/api/CQ', async (req, res) => {
+app.post('/ai/chat_bot', async (req, res) => {
+
+    const user_text = req.body.content
+
+    // Fetch the list of articles from the CMS
+    const response: api_return_schema<article[]> = await get_all_ready_articles()
+
+	if (response.error.has_error) {
+		res.status(500).json(response)
+		return
+	}
 
     var article_text_for_AI_consumption = [];
 
-    // Fetch the list of articles from the CMS
-    const response = await fetch(`${process.env.CMS_ROUTE}/get_all_ready_articles`);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const home_page_articles_JSON = await response.json(); // Assuming the response is JSON
-    const home_page_articles = home_page_articles_JSON.data
-
-    // Make sure to use 'let' or 'const' for variable declarations
-    for (let article of home_page_articles) { // Assuming 'home_page_articles' is an iterable array
-        // Fetch each article content based on the article source
-        const articleResponse = await fetch(`${process.env.CMS_ROUTE}/CMS/articles/${article.source}/article.mdx`);
-        if (!articleResponse.ok) {
-            throw new Error(`HTTP error! status: ${articleResponse.status}`);
+    for (let article of response.data) { 
+        const articleResponse = await fetch(article.article);
+        try {
+            if (articleResponse.ok) {
+                const articleText = await articleResponse.text(); 
+                article_text_for_AI_consumption.push(articleText);
+            }else{
+                throw new Error("Could not fetch article in /ai/chat_bot")
+            }
         }
-        const articleText = await articleResponse.text(); // Assuming you want the raw text
-        article_text_for_AI_consumption.push(articleText);
+        catch(e){
+            console.log(e)
+        }
+    }
+
+    const prompt_obj: api_return_schema<string> = get_chat_bot_system_prompt( article_text_for_AI_consumption.join('------ NEW ARTICLE -----') )
+    const prompt: string = prompt_obj.data
+
+    if( prompt_obj.error.has_error ){
+        res.status(500).json(prompt_obj)
+        return
     }
 
     try {
         const response = await anthropic.messages.create({
-            model: env.process.ANTHROPIC_MODE_LIGHT,
-            max_tokens: 500,
-            temperature: 0,
-            system: getSystemPrompt({whichPrompt:"CQ", articles_for_consumption:article_text_for_AI_consumption.join('------ NEW ARTICLE -----')}),
+            model: process.env.ANTHROPIC_MODE_LIGHT!,
+            max_tokens: 1024,
+            temperature: 70,
+            system: prompt,
             messages: [
                 {
                     role: "user",
-                    content: req.body.content
+                    content: user_text
                 }
             ]
         });
 
         res.status(200).json(response); // Send the API response back to the frontend
+    } catch (error) {
+        console.error('Error proxying request:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+app.post('/ai/tag_finder', async (req, res) => {
+
+    const user_text = req.body.content
+
+    const response: api_return_schema<chip[]> = await get_unique_chips()
+
+    if (response.error.has_error) {res.status(500).json(response); return}
+
+    const prompt_obj: api_return_schema<string> = get_tag_finder_system_prompt( response.data )
+    const prompt: string = prompt_obj.data
+
+    if( prompt_obj.error.has_error ){
+        res.status(500).json(prompt_obj)
+        return
+    }
+
+    try {
+        const response = await anthropic.messages.create({
+            model: "claude-3-haiku-20240307",
+            max_tokens: 500,
+            temperature: 0,
+            system: prompt,
+            messages: [
+                {
+                    role: "user",
+                    content: user_text
+                }
+            ]
+        });
+
+        res.json(response); // Send the API response back to the frontend
     } catch (error) {
         console.error('Error proxying request:', error);
         res.status(500).json({ message: 'Internal server error' });
